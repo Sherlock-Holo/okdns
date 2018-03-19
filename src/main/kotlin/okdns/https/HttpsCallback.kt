68798b8f5b866384c15b7578c38180abdf0e44eb
Sherlock Holo <sherlockya@gmail.com>
2018-03-20 00:10:34 +0800

    Set HttpsCallback deprecated

--- src/main/kotlin/okdns/https/HttpsCallback.kt
+++ src/main/kotlin/okdns/https/HttpsCallback.kt
@@ -8,6 +8,7 @@ import okhttp3.Response
 import java.io.IOException
 import java.net.InetAddress
 
+@Deprecated("useless class")
 class HttpsCallback(private val message: Message) : Callback {
     override fun onFailure(call: Call, e: IOException) {
         e.printStackTrace()
@@ -19,15 +20,17 @@ class HttpsCallback(private val message: Message) : Callback {
         if (json.Status != 0) return
         if (json.Answer == null) return
         json.Answer!!.forEach {
-            message.addAnswer(
-                    when (it.type) {
-                        1 -> Record.AAnswer(it.name!!, 1, it.TTL, InetAddress.getByName(it.data!!))
+            when (it.type) {
+                1 -> message.addAnswer(Record.AAnswer(it.name!!, 1, it.TTL, InetAddress.getByName(it.data!!)))
 
-                        28 -> Record.AAnswer(it.name!!, 1, it.TTL, InetAddress.getByName(it.data!!))
+                2 -> message.addNSRecord(Record.NSRecord(it.name!!, 1, it.TTL, it.data!!))
 
-                        else -> TODO("other type is not implement")
-                    }
-            )
+                5 -> message.addAnswer(Record.CNAMEAnswer(it.name!!, 1, it.TTL, it.data!!))
+
+                28 -> message.addAnswer(Record.AAnswer(it.name!!, 1, it.TTL, InetAddress.getByName(it.data!!)))
+
+                else -> TODO("other type is not implement")
+            }
         }
     }
 }
